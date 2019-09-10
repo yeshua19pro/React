24dcf5105a992f2a09fb4b4e66eae2f709212c7e
yeshua19pro <yeshuaangelito@gmail.com>
2019-09-10 01:11:31 -0500

    Agregando tipos de variables

---
+++ JSRepaso/JS/js/tiposDeVariables.js
@@ -0,0 +1,15 @@
+//Variables con var - const
+
+var pais = "Colombia";
+
+/*Una variable constante debbe ser inicialicada
+* Al ser una Constante sus valores nunca cambian
+*/
+const precio = 200;
+
+//Las variables tipo let son muy similares a las tipo "var"
+let temperatura;
+temperatura = 20;
+temperatura = "mil";
+
+console.log(temperatura);
