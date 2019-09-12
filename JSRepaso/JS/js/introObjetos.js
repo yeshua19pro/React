97411e77ebfe8b8b4ff6858f16a332acbd95454e
yeshua19pro <yeshuaangelito@gmail.com>
2019-09-12 01:53:28 -0500

    introduccion a objetos y constructor de objetos

---
+++ JSRepaso/JS/js/introObjetos.js
@@ -0,0 +1,13 @@
+//Objetos de manera literal
+
+const persona = {
+    nombre: 'Yashua',
+    rol: 'estudiante',
+    hijos: 0,
+    casado: false
+}
+
+console.log(persona);
+//diferentes formas de acceder a un valor especifico
+console.log(persona.hijos);
+console.log(persona['rol']);
---
+++ JSRepaso/JS/js/objectConstructor.js
@@ -0,0 +1,30 @@
+//Constructor de objetos
+
+function ConstructorEstudiantes(nombre, apellido){
+    this.nombre = nombre;
+    this.apellido = apellido;
+}
+
+const estudiante1 = new ConstructorEstudiantes('Camilo', 'Cuervo');
+const estudiante2 = new ConstructorEstudiantes('Mateo', 'Marulanda');
+const estudiante3 = new ConstructorEstudiantes('Yashua', 'Narvaez');
+console.log(estudiante1);
+console.log(estudiante2);
+console.log(estudiante3);
+
+/*Nota: El constructor de objetos es una funcion que recibe
+/varios parametros, sabemos que es un constructor porque utiliza
+/la palabra "this"
+*/
+
+function ConstructorEdificios(pisos, apartamentos){
+    this.pisos = pisos;
+    this.apartamentos = apartamentos;
+}
+
+let edificio1 = new ConstructorEdificios(10, 105);
+let edificio2 = new ConstructorEdificios(30, 320);
+let edificio3 = new ConstructorEdificios(15, 156);
+console.log(edificio1);
+console.log(edificio2);
+console.log(edificio3)
